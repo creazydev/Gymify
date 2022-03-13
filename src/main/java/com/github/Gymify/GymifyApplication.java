f8566c7f98e5bcc6dfc92a496b08076aa61ca688
creazydev <mlodyv111@gmail.com>
2022-03-13 14:50:26 +0100

    spring init

---
+++ src/main/java/com/github/Gymify/GymifyApplication.java
@@ -0,0 +1,13 @@
+package com.github.Gymify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Gymify/GymifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Gymify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
