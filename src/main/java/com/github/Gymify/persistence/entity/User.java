6251c6f80fecc21240bc06ef84f6b00c139ceab8
kamil.nowak <mlodyv111@gmail.com>
2022-03-19 13:23:32 +0100

    User details implementation

---
+++ src/main/java/com/github/Gymify/persistence/entity/User.java
@@ -0,0 +1,65 @@
+package com.github.Gymify.persistence.entity;
+
+import com.github.Gymify.persistence.enums.UserAuthority;
+import com.github.Gymify.persistence.type.EnumSetType;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+import java.util.EnumSet;
+
+@Entity
+@TypeDef(
+    name = "enum-set",
+    typeClass = EnumSetType.class
+)
+
+@Setter
+@Getter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+    @Type(type = "enum-set", parameters = {
+        @Parameter(name = "enumClass", value = "com.github.Gymify.persistence.enums.UserAuthority")
+    })
+    private EnumSet<UserAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
---
+++ src/main/java/com/github/Gymify/persistence/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.github.Gymify.persistence.repository;
+
+import com.github.Gymify.persistence.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String string);
+}
