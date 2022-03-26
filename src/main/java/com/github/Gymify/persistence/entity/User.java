3c969eb15426bfbddd421339d5234ab22f3566b9
creazydev <mlodyv111@gmail.com>
2022-03-26 08:51:06 +0100

    Jwt based security configuration. Replaced REST with GraphQL.

--- src/main/java/com/github/Gymify/persistence/entity/User.java
+++ src/main/java/com/github/Gymify/persistence/entity/User.java
@@ -3,7 +3,9 @@ package com.github.Gymify.persistence.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.Gymify.persistence.enums.UserAuthority;
 import com.github.Gymify.persistence.type.EnumSetType;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Type;
@@ -14,17 +16,23 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
 
 @Entity
+@Table(name = "application_user")
+
 @TypeDef(
     name = "enum-set",
     typeClass = EnumSetType.class
 )
 
+@NoArgsConstructor
 @Setter
 @Getter
 public class User implements UserDetails {
@@ -48,6 +56,16 @@ public class User implements UserDetails {
         @Parameter(name = "enumClass", value = "com.github.Gymify.persistence.enums.UserAuthority")
     })
     private EnumSet<UserAuthority> authorities;
+
+    @Builder
+    public User(String email, String password, Collection<UserAuthority> authorities) {
+        this.email = email;
+        this.password = password;
+        this.authorities = EnumSet.copyOf(authorities);
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.enabled = true;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
