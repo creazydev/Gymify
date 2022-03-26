b80ed51881311894693c90b72e87be9dc00aa74a
kamil.nowak <mlodyv111@gmail.com>
2022-03-26 16:28:21 +0100

    Sample user.

---
+++ src/main/java/com/github/Gymify/Bootstrap.java
@@ -0,0 +1,34 @@
+package com.github.Gymify;
+
+import com.github.Gymify.persistence.entity.User;
+import com.github.Gymify.persistence.enums.UserAuthority;
+import com.github.Gymify.persistence.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@Component
+@RequiredArgsConstructor
+public class Bootstrap {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    @Transactional
+    void addSampleUsers() {
+        Stream.of(
+            User.builder()
+                .email("test@mail.ru")
+                .password(passwordEncoder.encode("sample"))
+                .authorities(Set.of(UserAuthority.BASIC_USER))
+                .build()
+        )
+            .sequential()
+            .forEachOrdered(userRepository::save);
+    }
+}
