a6bd86d7e40f67fadfab62efe51d879118aa115b
kamil.nowak <mlodyv111@gmail.com>
2022-03-26 18:17:48 +0100

    Login page prototype based on MUI template.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SignIn from './component/SignIn';
+import {ApolloProvider} from "@apollo/client";
+import {client} from "./graphql/ApolloClient";
+
+const App = () => {
+    return (
+        <div>
+            <ApolloProvider client={client}>
+                <SignIn/>
+            </ApolloProvider>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/component/SignIn.js
@@ -0,0 +1,150 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {gql, useMutation} from "@apollo/client";
+import {AUTH_TOKEN} from "../authentication/constants";
+import {useState} from "react";
+
+const theme = createTheme();
+
+const LOGIN_MUTATION = gql`
+    mutation LoginMutation($email: String!$password: String!) {
+        login(email: $email, password: $password)
+    }
+`;
+
+export default function SignIn() {
+
+    const [formState, setFormState] = useState({
+        login: true,
+        email: '',
+        password: '',
+        name: ''
+    });
+
+    const [login] = useMutation(LOGIN_MUTATION, {
+        variables: {
+            email: formState.email,
+            password: formState.password
+        },
+        onError: (error) => {
+            console.log(error);
+        },
+        onCompleted: ({ login }) => {
+            console.log('xxx');
+            localStorage.setItem(AUTH_TOKEN, login.token);
+        }
+    });
+
+    const [signIn, { data, loading, error, reset }] = useMutation(LOGIN_MUTATION);
+
+    if (loading) {
+        console.log('xxx');
+    }
+
+    if (error) return `Submission error! ${error.message}`;
+
+    return (
+
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{height: '100vh'}}>
+                <CssBaseline/>
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                            <LockOutlinedIcon/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form"
+                             onSubmit={e => {
+                                 e.preventDefault();
+                                 console.log(e);
+                                 login(e).then();
+                             }}
+                             noValidate sx={{mt: 1}}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary"/>}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{mt: 3, mb: 2}}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
+}
