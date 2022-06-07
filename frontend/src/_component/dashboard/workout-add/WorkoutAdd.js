a2a5d9b1b0493010233d1b993af3c5bc3234bd5e
Szewo <szewczyk.mat@yahoo.com>
2022-06-07 20:33:25 +0200

    Add workout add page.

---
+++ frontend/src/_component/dashboard/workout-add/WorkoutAdd.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {useTranslation} from "react-i18next";
+
+const WorkoutAdd = () => {
+    const { t, i18n } = useTranslation();
+
+    return (
+        <div>
+            <h1>TODO</h1>
+        </div>
+    )
+};
+
+export default WorkoutAdd;
