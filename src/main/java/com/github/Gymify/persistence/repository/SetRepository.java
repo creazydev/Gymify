4e1b145c05b74da5bf37ef6731abe35fce0cad8a
kamil.nowak <mlodyv111@gmail.com>
2022-06-11 17:11:01 +0200

    SetService unit tests.

---
+++ src/main/java/com/github/Gymify/persistence/repository/SetRepository.java
@@ -0,0 +1,7 @@
+package com.github.Gymify.persistence.repository;
+
+import com.github.Gymify.persistence.entity.Exercise;
+import com.github.Gymify.persistence.entity.Set;
+
+public interface SetRepository extends UserResourceRepository<Set> {
+}
