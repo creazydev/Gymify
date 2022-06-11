41e8cc224049159afe8c3221ef5ccfe05544493e
kamil.nowak <mlodyv111@gmail.com>
2022-06-11 17:45:36 +0200

    Fixed updating set.

--- src/main/java/com/github/Gymify/core/service/SetService.java
+++ src/main/java/com/github/Gymify/core/service/SetService.java
@@ -2,6 +2,7 @@ package com.github.Gymify.core.service;
 
 import com.github.Gymify.exception.RuntimeExceptionWhileDataFetching;
 import com.github.Gymify.persistence.entity.Set;
+import com.github.Gymify.persistence.entity.WorkoutSession;
 import com.github.Gymify.persistence.repository.UserResourceRepository;
 import com.github.Gymify.persistence.specification.UserResourceSpecificationFactory;
 import com.github.Gymify.security.service.UserService;
@@ -15,6 +16,20 @@ public class SetService extends UserResourceService<Set> implements PrePersistEn
 
     public SetService(UserResourceRepository<Set> userResourceRepository, UserService userService, UserResourceSpecificationFactory<Set> specificationFactory) {
         super(userResourceRepository, userService, specificationFactory);
+    }
+
+    @Override
+    public Set update(Set set) {
+        return this.find(set.getId())
+            .stream()
+            .peek(savedSet -> {
+                savedSet.setRepsQuantity(set.getRepsQuantity());
+                savedSet.setPlannedRestDuration(set.getPlannedRestDuration());
+            })
+            .peek(this::validateEntity)
+            .map(super::update)
+            .findFirst()
+            .orElseGet(() -> this.add(set));
     }
 
     @Override
