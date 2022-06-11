b774ab35dabcee6cffd1c81cf2242b79ccf87383
Szewo <szewczyk.mat@yahoo.com>
2022-06-11 13:29:34 +0200

    Form for adding workouts

--- frontend/src/_component/dashboard/workout-add/WorkoutAdd.js
+++ frontend/src/_component/dashboard/workout-add/WorkoutAdd.js
@@ -1,12 +1,24 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
+import WorkoutAddForm from "./WorkoutAddForm";
+import Grid from "@mui/material/Grid";
+import WorkoutPlanTable from "../workout-plans/WorkoutPlanTable";
 
 const WorkoutAdd = () => {
     const { t, i18n } = useTranslation();
 
     return (
         <div>
-            <h1>TODO</h1>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+            >
+                <Grid item sx={3}>
+                    <WorkoutAddForm />
+                </Grid>
+            </Grid>
         </div>
     )
 };
