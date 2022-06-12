0bcb94f819005035c0dc821d7021b420a4f73b47
kamil.nowak <mlodyv111@gmail.com>
2022-06-12 13:08:55 +0200

    Added test db.

---
+++ src/test/java/com/github/Gymify/H2JpaConfig.java
@@ -0,0 +1,11 @@
+package com.github.Gymify;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.github.Gymify.persistence.repository")
+@EnableTransactionManagement
+public class H2JpaConfig {
+}
